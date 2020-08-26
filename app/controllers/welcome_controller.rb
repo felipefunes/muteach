5a450c6d1d2879f7c938d3d3d773abb10d04807b
Felipe Funes <felipefunes@gmail.com>
2020-08-26 19:43:12 -0400

    ADD: React configuration :D

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,5 @@
 class WelcomeController < ApplicationController
   def index
-    redirect_to users_home_index_path if user_signed_in?
+    redirect_to home_index_path if user_signed_in?
   end
 end
---
+++ app/javascript/packs/users_home.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+document.addEventListener("DOMContentLoaded", () => {
+  ReactDOM.render(
+    <div>Hola</div>,
+    document.getElementById("users-home")
+  );
+});
